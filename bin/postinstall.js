9a2b7f56ee7c4a5d958ed72c914fe180feab8371
newton <you@example.com>
2014-11-15 01:02:54 +0200

    Fixing binary

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,38 +1,29 @@
 #!/usr/bin/env node
 
 var fs = require("fs"),
-    path = require("path"),
-    exe = require('nodewebkit').findpath();
+    path = require("path");
 
-// If a party has r, add x
-// so that dirs are listable
 function dirMode(mode) {
-    if (mode & 0400) mode |= 0100
-    if (mode & 040) mode |= 010
-    if (mode & 04) mode |= 01
+    if (mode & 0400) mode |= 0100;
+    if (mode & 040) mode |= 010;
+    if (mode & 04) mode |= 01;
     return mode
 }
 
-var isAccessible = function (path) {
+function chmodrSync(p, mode) {
+    var children;
     try {
-        fs.readdirSync(path);
-    } catch (e) {
-        return false;
+        children = fs.readdirSync(p)
+    } catch (er) {
+        if (er && er.code === "ENOTDIR") return fs.chmodSync(p, mode);
+        throw er
     }
+    if (!children.length) return fs.chmodSync(p, dirMode(mode));
 
-    return true;
-};
+    children.forEach(function (child) {
+        chmodrSync(path.resolve(p, child), mode)
+    });
+    return fs.chmodSync(p, dirMode(mode))
+}
 
-var chmod = function (dir) {
-    if (isAccessible(dir)) return true;
-
-    var parent = path.dirname(dir);
-    if (parent !== dir && !isAccessible(parent)) {
-        chmod(parent);
-    }
-
-    console.log("CHMOD", dir, 755);
-    fs.chmodSync(dir, 755);
-};
-
-chmod(exe);
+chmodrSync(__dirname + '/../node_modules/nodewebkit/nodewebkit', 0755);
